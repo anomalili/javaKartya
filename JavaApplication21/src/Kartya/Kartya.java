1aa170c399557c5da518c44867aac309d49481bf
anomalili <makrazsofi@gmail.com>
2022-09-12 06:55:31 +0200

    alap osztálytulajdonságok

---
+++ JavaApplication21/src/Kartya/Kartya.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Kartya;
+
+/**
+ *
+ * @author makra
+ */
+public class Kartya {
+    
+    private String szin;
+    private String ertek;
+    
+}
